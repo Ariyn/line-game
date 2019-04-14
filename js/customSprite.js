4074fbd4fd1133f609a83d0cd7be63dcd083f80b
ariyn <himnowxz@gmail.com>
2019-04-15 00:36:33 +0900

    init commit

---
+++ js/customSprite.js
@@ -0,0 +1,37 @@
+var currentPosition = 0;
+var index = 0;
+let offsetX = 22
+let offsetY = -70;
+var size = [65, 65, 64, 63, 62, 61, 61, 61, 61, 61, 61, 61, 61, 62, 64, 65];
+
+function initSprite() {
+	currentPosition = -offsetX
+	$(".frame").css("background-position-x", currentPosition);
+	$(".frame").css("background-position-y", offsetY);
+}
+function animateScript() {
+	console.log(currentPosition, index, size[index])
+	currentPosition -= size[index];
+	if(index + 1)
+	$(".frame").css("background-position", currentPosition)
+}
+
+function animateNext() {
+	console.log(currentPosition, index-1, size[index-1])
+	currentPosition -= size[index];
+	$(".frame").css("background-position-x", currentPosition)
+	if(size.length <= index + 1) {
+		currentPosition = -offsetX
+		index = 0
+	} else {
+		index += 1
+	}
+}
+
+function animateBefore() {
+	if(index <= 0)
+		return false;
+	console.log(currentPosition, index-1, size[index-1])
+	currentPosition += size[--index];
+	$(".frame").css("background-position", currentPosition)
+}
